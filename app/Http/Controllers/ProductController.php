00512c75b274e1be44753c8506663e02cc385829
unknown <rud.hardianto@gmail.com>
2022-05-24 20:06:53 +0700

    add new product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductSingleResource;
+use Illuminate\Validation\ValidationException;
 
 class ProductController extends Controller
 {
@@ -14,9 +16,20 @@ class ProductController extends Controller
         return ProductResource::collection(Product::paginate(15));
     }
 
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        if ($request->price < 10000) {
+            throw ValidationException::withMessages([
+                'price' => ['The price must be greater than 10000'],
+            ]);
+        }
+
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'message' => 'Product created successfully',
+            'product' => new ProductSingleResource($product),
+        ]);
     }
 
     public function show(Product $product)
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,22 @@
 namespace App\Models;
 
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function (Product $product) {
+            $product->slug = strtolower(Str::slug($product->name . '-' . Str::random(5)));
+        });
+    }
+
+    protected $guarded = [];
 
     public function getRouteKeyName()
     {
