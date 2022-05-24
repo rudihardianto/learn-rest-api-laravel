a6fa8ff762160567312b2a1b716d1ebf58ebcbd4
unknown <rud.hardianto@gmail.com>
2022-05-24 10:42:56 +0700

    routing, controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,11 @@ class Product extends Model
 {
     use HasFactory;
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
