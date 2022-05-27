30b8813636b9add432eecd8ca12467738f9f29e2
unknown <rud.hardianto@gmail.com>
2022-05-27 08:50:14 +0700

    delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,6 +46,10 @@ class ProductController extends Controller
 
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json([
+            'message' => 'Product deleted successfully',
+        ]);
     }
 }
